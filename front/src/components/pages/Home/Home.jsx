dc0fab048492ae172b3b96eda2eb5423b59f513c
kai-nn <kai.nn@mail.ru>
2022-08-18 21:51:59 +0300

    Добавил шаблон Главной страницы

---
+++ front/src/components/pages/Home/Home.jsx
@@ -0,0 +1,45 @@
+import React, {useState} from "react";
+import {content} from './content'
+import style from './Home.module.css'
+
+const Home = () => {
+
+    const [active, setActive] = useState(0)
+
+    const sellect = (value) =>{
+        console.log(value)
+        setActive(value)
+    }
+
+
+    return (
+        <>
+            <div className={style.background}></div>
+
+            <div className={style.foundation}>
+                <div className={style.menu}>
+                    {
+                        content.map((el, ind) => {
+                            return (
+                                <div className={ind === active ? style.el_menu__active : style.el_menu}
+                                    onClick={() => sellect(ind)}
+                                     key={ind} >
+                                    {el.elementMenu}
+                                </div>
+                            )
+                        })
+                    }
+                </div>
+
+                <div className={style.description}>
+                    {
+                        content.find((el, ind) => ind === active).body
+                    }
+                </div>
+            </div>
+
+        </>
+    )
+}
+
+export default Home
---
+++ front/src/components/pages/Home/content.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+export const content = [
+    {
+        elementMenu: 'О сервисе',
+        body:
+            <div>
+                <h3>О сервисе</h3>
+            </div>
+    },
+
+    {
+        elementMenu: 'Демонстрация',
+        body: <div>Демонстрация</div>
+    },
+
+    {
+        elementMenu: 'Отзывы пользователей',
+        body: <div>Отзывы пользователей</div>
+    },
+
+    {
+        elementMenu: 'Как подключиться',
+        body: <div>Как подключиться</div>
+    },
+
+    {
+        elementMenu: 'Об авторе',
+        body: <div>Об авторе</div>
+    },
+]
