4f0533a2f26c57e71a8a865a339b0141ef56344b
kai-nn <kai.nn@mail.ru>
2022-09-10 21:07:17 +0300

    Наполнил контентом главную страницу, favicon.ico, ...

--- front/src/components/pages/TestJWT/TestJWT.jsx
+++ front/src/components/pages/TestJWT/TestJWT.jsx
@@ -1,26 +1,21 @@
 import React, {useState} from 'react'
 import style from './TestJWT.module.scss'
-import {Avatar} from "@mui/material";
-import BurgerToggle from "../../BurgerToggle/BurgerToggle";
+import axios from "axios";
+
 
 
 const TestJWT = () => {
 
 
-
     return (
-        <div>
-        <br />
-
-        <div className={style.parrent}>
-            <div className={style.child}>
-                <div>123</div>
-                <div className={style.round}></div>
+        <>
+            <div className={style.logo}>
+                <div className={style.logo_micro}>M</div>
+                <div className={style.logo_erp}>E</div>
+                <div className={style.logo_new}>N</div>
+                <div className={style.logo_generation}>G</div>
             </div>
-        </div>
-
-
-        </div>
+        </>
     )
 }
 
